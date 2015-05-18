6480dcd60e79e92cb1a6dcbba2032dacbb5d3a32
ffegert2011 <ffegert2011@my.fit.edu>
2015-05-17 20:57:15 -0400

    2nd change to hello git

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, Git!");
+		
+		System.out.println("I am adding more so I can commit practice.");
 	}
 
 }
